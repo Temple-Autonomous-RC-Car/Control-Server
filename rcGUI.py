9293839ed1f85c5f6e0da3bdcffd3daa905afdaf
mattcaraan <tuf35360@temple.edu>
2018-11-02 16:24:46 -0400

    Car Logging Works with Test Strings

--- rcGUI.py
+++ rcGUI.py
@@ -1,10 +1,12 @@
 import sys
 import cv2
 import PyQt5
-from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtCore import pyqtSlot, QTimer
 from PyQt5.QtWidgets import QApplication,QDialog,QLabel
 from PyQt5.uic import loadUi
 from PyQt5.QtGui import QIcon, QPixmap
+
+import testLog
 
 #Car Gui opens when executed
 class loadCarGui(QDialog):
@@ -13,14 +15,35 @@ class loadCarGui(QDialog):
         loadUi('rcGUI_design.ui',self)
         self.setWindowTitle('Neural Network R/C Car')
         self.ConnectButton.clicked.connect(self.connectionButtonClicked)
+        self.testLogButton.clicked.connect(self.testLogFeed)
+        self.stopLogButton.clicked.connect(self.stopLogFeed)
         self.TestCamButton.clicked.connect(self.Start_WebCam)
         self.TurnOffCamButton.clicked.connect(self.Stop_WebCam)
+        self.i = 0
+        self.qTimer = QTimer()
+        self.qTimer.setInterval(500)
+        self.qTimer.timeout.connect(self.testLogFeed)
+        self.temp = ""
+        #self.qTimer.start()
     @pyqtSlot()
 
 #------------------------------------------------------------------------------#
 #This function will take IP Address and connect to the PI
     def connectionButtonClicked(self):
-        self.CarLogLabel.setText('Test Log: '+self.IPlineEdit.text())
+        self.CarIPLabel.setText(self.IPlineEdit.text())
+
+#------------------------------------------------------------------------------#
+#This function will test output sensor log from testLog.py
+    def testLogFeed(self):
+        #self.CarLogLabel.setText(testLog.testPrint())
+        self.qTimer.start()
+        self.i +=1
+        self.temp += testLog.testPrint() + str(self.i) + "\n"
+        self.CarLogLabel.setText(self.temp)
+
+    def stopLogFeed(self):
+        self.qTimer.stop()
+
 
 #------------------------------------------------------------------------------#
 # Code to Test WebCam Functionality.
--- testLog.py
+++ testLog.py
@@ -1,7 +1,5 @@
 import threading
 
 def testPrint():
-    threading.Timer(1.0, testPrint).start()
-    self.CarLogLabel.setText(self.text("Hello, World!"))
-
-testPrint()
+    #threading.Timer(1.0, testPrint).start()
+    return "Hello, World!"
