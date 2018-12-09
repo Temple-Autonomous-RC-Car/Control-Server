b59da7522485a68aa0df9f0592730a4a37cf390f
Matthew Caraan <matt2caraan@gmail.com>
2018-12-09 18:28:30 -0500

    Added Try-Catch Blocks with working error label

--- rcGUI.py
+++ rcGUI.py
@@ -5,7 +5,7 @@ from PyQt5.QtCore import pyqtSlot, QTimer
 from PyQt5.QtWidgets import *
 from PyQt5.uic import loadUi
 from PyQt5.QtGui import QIcon, QPixmap
-from win32api import GetSystemMetrics
+#from win32api import GetSystemMetrics
 import os
 import queue
 
@@ -29,6 +29,11 @@ class loadCarGui(QDialog):
         self.sensorLog_verticalScrollBar.setValue(self.sensorLog_verticalScrollBar.maximum());
         self.scrollArea.setWidget(self.CarLogLabel)
 
+        self.err_scrollArea.setWidget(self.exception_line)
+        self.err_scrollArea.setVerticalScrollBar(self.err_verticalScrollBar)
+        self.err_verticalScrollBar.setValue(self.err_verticalScrollBar.maximum());
+        self.err_scrollArea.setWidget(self.exception_line)
+
         self.sendCMD.clicked.connect(self.socketTextTest)
         self.MapUploadButton.clicked.connect(self.mapUpload)
         self.ConnectButton.clicked.connect(self.connectionButtonClicked)
@@ -47,91 +52,125 @@ class loadCarGui(QDialog):
         self.qTimer.setInterval(500)
         self.qTimer.timeout.connect(self.testLogFeed)
         self.temp = ""
+        self.err = ""
 
 
         #self.qTimer.start()
     @pyqtSlot()
 
+
+
+#------------------------------------------------------------------------------#
+    def errorCatch(self, s):
+        self.err += s + "\n"
+        self.exception_line.setText(self.err)
+        self.err_verticalScrollBar.setValue(self.err_verticalScrollBar.maximum());
+
+
+
 #------------------------------------------------------------------------------#
 #This function will connected to the socket-man.py listener socket
     def socketTextTest(self):
-        line = self.cmd_line.toPlainText()
-        socket_man_test.pullfromCLient(line)
+        try:
+            line = self.cmd_line.toPlainText()
+            socket_man_test.pullfromCLient(line)
+        except:
+            self.errorCatch("Error in method socketTextTest")
+
 
 #------------------------------------------------------------------------------#
 #This function will take map file and send it to processing. Then show processed image
 #   on MapProccesedImage
     def mapUpload(self):
-        #import find_corners
+        try:
+            #import find_corners
 
-        # Create widget
-        #label = QLabel(self.MapProccesedImage)
-        pixmap = QPixmap('roomwithoutmarkers.png')
-        resized_pixmap = pixmap.scaled(574.67, 431)
-        self.MapProccesedImage.setPixmap(resized_pixmap)
-        #self.resize(pixmap.width(),pixmap.height())
-        #self.MapProccesedImage.displayImage("finalWithNodes.png", 1)
+            # Create widget
+            #label = QLabel(self.MapProccesedImage)
+            pixmap = QPixmap('roomwithoutmarkers.png')
+            resized_pixmap = pixmap.scaled(574.67, 431)
+            self.MapProccesedImage.setPixmap(resized_pixmap)
+            #self.resize(pixmap.width(),pixmap.height())
+            #self.MapProccesedImage.displayImage("finalWithNodes.png", 1)
+        except:
+            self.errorCatch("Error in method mapUpload")
 
     def pickXcoordinate(self):
-        print("Picking Starting Coordinate...")
-        #set mouse callback function for window
-        #this function will be called whenever the mouse is right-clicked
-        def mouse_callback(event, x, y, flags, params):
-            global startingX, startingY
-            #right-click event value is 2
-            if event == 2:
+        try:
+            print("Picking Starting Coordinate...")
+            #set mouse callback function for window
+            #this function will be called whenever the mouse is right-clicked
+            def mouse_callback(event, x, y, flags, params):
+                try:
+                    global startingX, startingY
+                    #right-click event value is 2
+                    if event == 2:
 
-                #store the coordinates of the right-click event in list
-                #right_clicks.append([x, y])
+                        #store the coordinates of the right-click event in list
+                        #right_clicks.append([x, y])
 
-                #this just verifies that the mouse data is being collected
-                #you probably want to remove this later
-                #print(right_clicks)
-                startingX,startingY = x,y
-                print(startingX,startingY)
-                self.Startcoord_textEdit.setText("X: " + str(startingX) + " Y: " + str(startingY))
-        img = cv2.imread("roomwithoutmarkers.png",0)
-        height = img.shape[0]
-        width = img.shape[1]
-        cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-        cv2.resizeWindow('image', width, height)
-        cv2.imshow('image', img)
-        cv2.setMouseCallback('image', mouse_callback)
+                        #this just verifies that the mouse data is being collected
+                        #you probably want to remove this later
+                        #print(right_clicks)
+                        startingX,startingY = x,y
+                        print(startingX,startingY)
+                        self.Startcoord_textEdit.setText("X: " + str(startingX) + " Y: " + str(startingY))
+                except:
+                    self.errorCatch("Error in method mouse_callback for X")
+
+            img = cv2.imread("roomwithoutmarkers.png",0)
+            height = img.shape[0]
+            width = img.shape[1]
+            cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+            cv2.resizeWindow('image', width, height)
+            cv2.imshow('image', img)
+            cv2.setMouseCallback('image', mouse_callback)
+        except:
+            self.errorCatch("Error in method pickXcoordinate")
 
     def pickYcoordinate(self):
-        print("Picking Ending Coordinate...")
-        #set mouse callback function for window
-        #this function will be called whenever the mouse is right-clicked
-        def mouse_callback(event, x, y, flags, params):
-            global endingX, endingY
-            #right-click event value is 2
-            if event == 2:
+        try:
+            print("Picking Ending Coordinate...")
+            #set mouse callback function for window
+            #this function will be called whenever the mouse is right-clicked
+            def mouse_callback(event, x, y, flags, params):
+                try:
+                    global endingX, endingY
+                    #right-click event value is 2
+                    if event == 2:
 
-                #store the coordinates of the right-click event in list
-                #right_clicks.append([x, y])
+                        #store the coordinates of the right-click event in list
+                        #right_clicks.append([x, y])
 
-                #this just verifies that the mouse data is being collected
-                #you probably want to remove this later
-                #print(right_clicks)
-                endingX,endingY = x,y
-                print(endingX,endingY)
-                self.Endcoord_textEdit.setText("X: " + str(endingX) + " Y: " + str(endingY))
-        img = cv2.imread("roomwithoutmarkers.png",0)
-        height = img.shape[0]
-        width = img.shape[1]
-        cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-        cv2.resizeWindow('image', width, height)
-        cv2.imshow('image', img)
-        cv2.setMouseCallback('image', mouse_callback)
+                        #this just verifies that the mouse data is being collected
+                        #you probably want to remove this later
+                        #print(right_clicks)
+                        endingX,endingY = x,y
+                        print(endingX,endingY)
+                        self.Endcoord_textEdit.setText("X: " + str(endingX) + " Y: " + str(endingY))
+                except:
+                    errorCatch("Error in method mouse_caalback for Y")
+            img = cv2.imread("roomwithoutmarkers.png",0)
+            height = img.shape[0]
+            width = img.shape[1]
+            cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+            cv2.resizeWindow('image', width, height)
+            cv2.imshow('image', img)
+            cv2.setMouseCallback('image', mouse_callback)
+        except:
+            self.errorCatch("Error in method pickYcoordinate")
 
 
     def sendToMapProcessingCode(self):
-        os.system("find_corners.py " + str(startingX) + " " + str(startingY) + " " + str(endingX) + " " + str(endingY))
+        try:
+            os.system("find_corners.py " + str(startingX) + " " + str(startingY) + " " + str(endingX) + " " + str(endingY))
 
-        #q = queue.Queue()
-        pixmap = QPixmap('finalWithNodes.png')
-        resized_pixmap = pixmap.scaled(574.67, 431)
-        self.MapProccesedImage.setPixmap(resized_pixmap)
+            #q = queue.Queue()
+            pixmap = QPixmap('finalWithNodes.png')
+            resized_pixmap = pixmap.scaled(574.67, 431)
+            self.MapProccesedImage.setPixmap(resized_pixmap)
+        except:
+            self.errorCatch("Error in method sendToMapProcessingCode")
 
 
 
@@ -139,34 +178,37 @@ class loadCarGui(QDialog):
 #------------------------------------------------------------------------------#
 #This function will take IP Address and connect to the PI
     def connectionButtonClicked(self):
-
-        #WILL CONNECT TO SERVER
-        import socket_man_test
-        self.CarIPLabel.setText(self.IPlineEdit.text())
-        ipVAl = self.IPlineEdit.text()
-        if ipVAl == "":
-            self.noIPwarning.setText("NO IP:PORT VALUE")
+        try:
+            #WILL CONNECT TO SERVER
+            import socket_man_test
+            self.CarIPLabel.setText(self.IPlineEdit.text())
+            ipVAl = self.IPlineEdit.text()
+            if ipVAl == "":
+                self.noIPwarning.setText("NO IP:PORT VALUE")
+        except:
+            self.errorCatch("Error in method connectionButtonClicked")
 
 
 #------------------------------------------------------------------------------#
 #This function will test output sensor log from testLog.pyr
     def testLogFeed(self):
+        try:
 
+            #self.CarLogLabel.setText(testLog.testPrint())
+            self.qTimer.start()
 
-        #self.CarLogLabel.setText(testLog.testPrint())
-        self.qTimer.start()
+            self.i += 1
+            self.temp += testLog.testPrint() + " " + str(self.i) + "\n"
+            self.CarLogLabel.setText(self.temp)
+            self.sensorLog_verticalScrollBar.setValue(self.sensorLog_verticalScrollBar.maximum());
 
-        self.i += 1
-        self.temp += testLog.testPrint() + " " + str(self.i) + "\n"
-        self.CarLogLabel.setText(self.temp)
-        self.sensorLog_verticalScrollBar.setValue(self.sensorLog_verticalScrollBar.maximum());
+            #Find a way to keep calling the scrollbar.maximum()
 
-        #Find a way to keep calling the scrollbar.maximum()
-
-        #self.sensorLog_verticalScrollBar.setValue(scrollBar.maxValue())
-        #while self.qTimer.stop() != 'true':
-        #    self.sensorLog_verticalScrollBar.maximum();
-
+            #self.sensorLog_verticalScrollBar.setValue(scrollBar.maxValue())
+            #while self.qTimer.stop() != 'true':
+            #    self.sensorLog_verticalScrollBar.maximum();
+        except:
+            self.errorCatch("Error in method testLogFeed")
 
 
     def stopLogFeed(self):
@@ -178,26 +220,29 @@ class loadCarGui(QDialog):
 # Code to Test WebCam Functionality.
 # This is to just figure out how live video will work on this client
     def Start_WebCam(self):
-        #port on webcam
-        #self.capture=cv2.VideoCapture(0)
+        try:
+            #port on webcam
+            #self.capture=cv2.VideoCapture(0)
 
-        #Port on the Aaron's pi
-        #self.capture=cv2.VideoCapture('http://192.168.137.141:8081/')
+            #Port on the Aaron's pi
+            #self.capture=cv2.VideoCapture('http://192.168.137.141:8081/')
 
-        #Function will return if IP:Port Value is empty
-        if self.IPlineEdit.text() == "":
-            self.noIPwarning.setText("NO IP:PORT VALUE")
-            return
+            #Function will return if IP:Port Value is empty
+            if self.IPlineEdit.text() == "":
+                self.noIPwarning.setText("NO IP:PORT VALUE")
+                return
 
-        #IP:Port from input
-        self.capture=cv2.VideoCapture('http://' + self.IPlineEdit.text() + '/')
+            #IP:Port from input
+            self.capture=cv2.VideoCapture('http://' + self.IPlineEdit.text() + '/')
 
-        self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-        self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+            self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+            self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 
-        self.timer=PyQt5.QtCore.QTimer(self)
-        self.timer.timeout.connect(self.update_frame)
-        self.timer.start(5)
+            self.timer=PyQt5.QtCore.QTimer(self)
+            self.timer.timeout.connect(self.update_frame)
+            self.timer.start(5)
+        except:
+            self.errorCatch("Error in method Start_WebCam")
 
     def update_frame(self):
         ret,self.image=self.capture.read()
@@ -205,27 +250,32 @@ class loadCarGui(QDialog):
         self.displayImage(self.image,1)
 
     def Stop_WebCam(self):
-        self.timer.stop()
-        self.LiveCamView.clear()
-        self.capture.release()
-        cv2.destroyAllWindows()
+        try:
+            self.timer.stop()
+            self.LiveCamView.clear()
+            self.capture.release()
+            cv2.destroyAllWindows()
+        except:
+            self.errorCatch("Error in method Stop_WebCam")
 
     def displayImage(self,img,window=1):
-        qformat=PyQt5.QtGui.QImage.Format_Indexed8
-        if len(img.shape)==3 : #[0]=rows , [1]=cols [2]=channels
-            if img.shape[2]==4 :
-                qformat=PyQt5.QtGui.QImage.Format_RGBA8888
-            else:
-                qformat=PyQt5.QtGui.QImage.Format_RGB888
+        try:
+            qformat=PyQt5.QtGui.QImage.Format_Indexed8
+            if len(img.shape)==3 : #[0]=rows , [1]=cols [2]=channels
+                if img.shape[2]==4 :
+                    qformat=PyQt5.QtGui.QImage.Format_RGBA8888
+                else:
+                    qformat=PyQt5.QtGui.QImage.Format_RGB888
 
-        outImage=PyQt5.QtGui.QImage(img,img.shape[1],img.shape[0],img.strides[0],qformat)
-        #BGR>>RGB
-        outImage=outImage.rgbSwapped()
+            outImage=PyQt5.QtGui.QImage(img,img.shape[1],img.shape[0],img.strides[0],qformat)
+            #BGR>>RGB
+            outImage=outImage.rgbSwapped()
 
-        if window==1:
-            self.LiveCamView.setPixmap(QPixmap.fromImage(outImage))
-            self.LiveCamView.setScaledContents(True)
-
+            if window==1:
+                self.LiveCamView.setPixmap(QPixmap.fromImage(outImage))
+                self.LiveCamView.setScaledContents(True)
+        except:
+            self.errorCatch("Error in method displayImage")
 #-----------------------Live Video Feed Code Ends Here-------------------------#
 
     def exitGUI(self):
