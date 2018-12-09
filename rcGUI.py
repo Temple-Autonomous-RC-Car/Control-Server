26d6ddf76eccbe9ae1b5664355775c053c833138
Matthew Caraan <matt2caraan@gmail.com>
2018-12-09 16:02:41 -0500

    12/9 Changes: Map Software Integrated into Client

--- rcGUI.py
+++ rcGUI.py
@@ -5,50 +5,143 @@ from PyQt5.QtCore import pyqtSlot, QTimer
 from PyQt5.QtWidgets import *
 from PyQt5.uic import loadUi
 from PyQt5.QtGui import QIcon, QPixmap
+from win32api import GetSystemMetrics
+import os
+import queue
 
 import testLog
-import socket_man_test
+
+#GLOBAL VARIABLES FOR MAP COORDINATES
+#right_clicks = list()
+startingX, startingY = -1,-1
+endingX, endingY = -1,-1
 
 #Car Gui opens when executed
 class loadCarGui(QDialog):
     def __init__(self):
         super(loadCarGui,self).__init__()
         loadUi('rcGUI_design.ui',self)
-
-
-
         self.setWindowTitle('Neural Network R/C Car')
+
+
         self.scrollArea.setWidget(self.CarLogLabel)
         self.scrollArea.setVerticalScrollBar(self.sensorLog_verticalScrollBar)
         self.sensorLog_verticalScrollBar.setValue(self.sensorLog_verticalScrollBar.maximum());
-
-        self.scrollArea.setWidget(self.CarLogLabel) 
+        self.scrollArea.setWidget(self.CarLogLabel)
 
         self.sendCMD.clicked.connect(self.socketTextTest)
-
-
+        self.MapUploadButton.clicked.connect(self.mapUpload)
         self.ConnectButton.clicked.connect(self.connectionButtonClicked)
         self.testLogButton.clicked.connect(self.testLogFeed)
         self.stopLogButton.clicked.connect(self.stopLogFeed)
         self.TestCamButton.clicked.connect(self.Start_WebCam)
         self.TurnOffCamButton.clicked.connect(self.Stop_WebCam)
+
+
+        self.pickX.clicked.connect(self.pickXcoordinate)
+        self.pickY.clicked.connect(self.pickYcoordinate)
+        self.sendToMapProcessingButton.clicked.connect(self.sendToMapProcessingCode)
+
         self.i = 0
         self.qTimer = QTimer()
         self.qTimer.setInterval(500)
         self.qTimer.timeout.connect(self.testLogFeed)
         self.temp = ""
+
+
         #self.qTimer.start()
     @pyqtSlot()
 
-
 #------------------------------------------------------------------------------#
+#This function will connected to the socket-man.py listener socket
     def socketTextTest(self):
         line = self.cmd_line.toPlainText()
         socket_man_test.pullfromCLient(line)
 
 #------------------------------------------------------------------------------#
+#This function will take map file and send it to processing. Then show processed image
+#   on MapProccesedImage
+    def mapUpload(self):
+        #import find_corners
+
+        # Create widget
+        #label = QLabel(self.MapProccesedImage)
+        pixmap = QPixmap('roomwithoutmarkers.png')
+        resized_pixmap = pixmap.scaled(574.67, 431)
+        self.MapProccesedImage.setPixmap(resized_pixmap)
+        #self.resize(pixmap.width(),pixmap.height())
+        #self.MapProccesedImage.displayImage("finalWithNodes.png", 1)
+
+    def pickXcoordinate(self):
+        print("Picking Starting Coordinate...")
+        #set mouse callback function for window
+        #this function will be called whenever the mouse is right-clicked
+        def mouse_callback(event, x, y, flags, params):
+            global startingX, startingY
+            #right-click event value is 2
+            if event == 2:
+
+                #store the coordinates of the right-click event in list
+                #right_clicks.append([x, y])
+
+                #this just verifies that the mouse data is being collected
+                #you probably want to remove this later
+                #print(right_clicks)
+                startingX,startingY = x,y
+                print(startingX,startingY)
+                self.Startcoord_textEdit.setText("X: " + str(startingX) + " Y: " + str(startingY))
+        img = cv2.imread("roomwithoutmarkers.png",0)
+        height = img.shape[0]
+        width = img.shape[1]
+        cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('image', width, height)
+        cv2.imshow('image', img)
+        cv2.setMouseCallback('image', mouse_callback)
+
+    def pickYcoordinate(self):
+        print("Picking Ending Coordinate...")
+        #set mouse callback function for window
+        #this function will be called whenever the mouse is right-clicked
+        def mouse_callback(event, x, y, flags, params):
+            global endingX, endingY
+            #right-click event value is 2
+            if event == 2:
+
+                #store the coordinates of the right-click event in list
+                #right_clicks.append([x, y])
+
+                #this just verifies that the mouse data is being collected
+                #you probably want to remove this later
+                #print(right_clicks)
+                endingX,endingY = x,y
+                print(endingX,endingY)
+                self.Endcoord_textEdit.setText("X: " + str(endingX) + " Y: " + str(endingY))
+        img = cv2.imread("roomwithoutmarkers.png",0)
+        height = img.shape[0]
+        width = img.shape[1]
+        cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('image', width, height)
+        cv2.imshow('image', img)
+        cv2.setMouseCallback('image', mouse_callback)
+
+
+    def sendToMapProcessingCode(self):
+        os.system("find_corners.py " + str(startingX) + " " + str(startingY) + " " + str(endingX) + " " + str(endingY))
+
+        #q = queue.Queue()
+        pixmap = QPixmap('finalWithNodes.png')
+        resized_pixmap = pixmap.scaled(574.67, 431)
+        self.MapProccesedImage.setPixmap(resized_pixmap)
+
+
+
+
+#------------------------------------------------------------------------------#
 #This function will take IP Address and connect to the PI
     def connectionButtonClicked(self):
+
+        #WILL CONNECT TO SERVER
+        import socket_man_test
         self.CarIPLabel.setText(self.IPlineEdit.text())
         ipVAl = self.IPlineEdit.text()
         if ipVAl == "":
@@ -134,6 +227,10 @@ class loadCarGui(QDialog):
             self.LiveCamView.setScaledContents(True)
 
 #-----------------------Live Video Feed Code Ends Here-------------------------#
+
+    def exitGUI(self):
+        sys.exit()
+
 
 
 if __name__=='__main__':
--- socket_man_test.py
+++ socket_man_test.py
@@ -1,8 +1,8 @@
 import socket
 import sys
 
-TCP_IP = '10.42.0.217'
-#TCP_IP = '127.0.0.1'
+#TCP_IP = '10.42.0.217'
+TCP_IP = '127.0.0.1'
 TCP_PORT = 12346
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
